f4b453716231cf5987c2dbc9ebd9c328eb607851
skid901 <tlrudejr1993@gmail.com>
2021-05-05 03:12:06 +0900

    feat: add config moduel and typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import configuration from './config/configuration';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      envFilePath: ['.env.development', '.env.production', '.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.database'),
+        entities: [join(__dirname, '/**/*.entity.js')],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/configuration.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+
+export default () => ({
+  database: {
+    host: process.env.DB_HOST ?? '',
+    port: process.env.DB_PORT ?? '',
+    username: process.env.DB_USERNAME ?? '',
+    password: process.env.DB_PASSWORD ?? '',
+    database: process.env.DB_DATABASE ?? '',
+  },
+});
