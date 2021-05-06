71a9a2f48c1b35ac475ea47dff29223a308ef209
skid901 <tlrudejr1993@gmail.com>
2021-05-07 01:40:07 +0900

    feat: add redis adapter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { RedisIoAdapter } from './adapters/redis.adapter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useWebSocketAdapter(new RedisIoAdapter());
+
   await app.listen(3000);
 }
 bootstrap();
