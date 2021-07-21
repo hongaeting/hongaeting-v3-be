12046a2a8e2d27f1ae49dbaf8051825288e4cc4b
skid901 <tlrudejr1993@gmail.com>
2021-07-21 22:22:26 +0900

    feat: set up SSL

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,29 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as dotenv from 'dotenv';
 
 import { RedisIoAdapter } from './adapters/redis.adapter';
 import { AppModule } from './app.module';
 
+dotenv.config();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync(
+      path.join(__dirname, '..', ...(process.env?.SSL_KEY ?? '').split('/')),
+      'utf8',
+    ),
+    cert: fs.readFileSync(
+      path.join(__dirname, '..', ...(process.env?.SSL_CERT ?? '').split('/')),
+      'utf8',
+    ),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: process.env.NODE_ENV === 'development' ? {} : httpsOptions,
+  });
   const configService = app.get(ConfigService);
 
   app.setGlobalPrefix(`api/${configService.get('API_VERSION')}`);
